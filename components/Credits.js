7e99eb8a4c1d4691b791478899ed78cb5e675005
Thomas Bouder <9974362+TBouder@users.noreply.github.com>
2021-08-23 15:29:44 +0200

    Update Credits.js

--- components/Credits.js
+++ components/Credits.js
@@ -1,10 +1,3 @@
-/******************************************************************************
-**	@Author:				Thomas Bouder <Tbouder>
-**	@Email:					Tbouder@protonmail.com
-**	@Date:					Sunday August 8th 2021
-**	@Filename:				Credits.js
-******************************************************************************/
-
 import	React	from	'react';
 
 function parseMarkdown(markdownText) {
@@ -20,7 +13,7 @@ function	Credits() {
 			<div className={'items-center justify-center flex flex-row w-full'}>
 				<div className={'flex flex-row items-center justify-center space-x-6 divide-x-2 divide-gray-200'}>
 					<div className={'flex flex-row justify-center space-x-2 group'}>
-						<p className={'text-ygray-500 text-xs'} dangerouslySetInnerHTML={{__html: parseMarkdown(process.env.CREDITS)}} />
+						<p className={'text-ygray-500 text-xs'}>{'By 🔵 '}&nbsp;{'with 💙'}}</p>
 					</div>
 				</div>
 			</div>

e345df72724990812d7f12f91e2d27c36ab3b3e7
Thomas Bouder <9974362+TBouder@users.noreply.github.com>
2021-08-23 15:29:54 +0200

    Update Credits.js

--- components/Credits.js
+++ components/Credits.js
@@ -13,7 +13,7 @@ function	Credits() {
 			<div className={'items-center justify-center flex flex-row w-full'}>
 				<div className={'flex flex-row items-center justify-center space-x-6 divide-x-2 divide-gray-200'}>
 					<div className={'flex flex-row justify-center space-x-2 group'}>
-						<p className={'text-ygray-500 text-xs'}>{'By 🔵 '}&nbsp;{'with 💙'}}</p>
+						<p className={'text-ygray-500 text-xs'}>{'By 🔵 '}&nbsp;{'with 💙'}</p>
 					</div>
 				</div>
 			</div>
